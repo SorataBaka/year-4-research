65b4d9a9fe444569f8485014c66ed8ace65a27c8
Christian Harjuno <harjuno.tian@gmail.com>
2024-04-30 12:25:39 +0900

    face and eye detection in multiple camera feed

--- blink_detect_v2/main.py
+++ blink_detect_v2/main.py
@@ -3,10 +3,11 @@ import numpy as np
 
 camera = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
 
 while True:
   ret, frame = camera.read()
+  frame = cv2.flip(frame, 1)
   gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
   gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
   gray = cv2.bilateralFilter(gray, 5, 1, 1)
@@ -20,13 +21,15 @@ while True:
       eyeFrame = np.copy(faceFrame)
       roi_face = gray[y:y+h,x:x+w]
       roi_face_clr = frame[y:y+h,x:x+w]
-      eyes = eye_cascade.detectMultiScale(roi_face,1.5,minSize=(50,50))
+      eyes = eye_cascade.detectMultiScale(roi_face,1.3,minSize=(50,50))
       if(len(eyes) > 0):
         for (eyeX, eyeY, eyeW, eyeH) in eyes:
           eyeFrame = cv2.rectangle(eyeFrame, (eyeX + x, eyeY + y), (eyeX + x + eyeW, eyeY + y +  eyeH), (255,255, 0), 2)
-        
-        
-
+          eyeIsolate = roi_face[eyeY:eyeY+eyeH, eyeX:eyeX+eyeW]
+          cv2.imshow("EYE", eyeIsolate)
+      else:
+        print("Maybe blink")
+          
   cv2.putText(frame, "Original Stream", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 5)
   cv2.putText(gray, "Grayscale", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 5)
   cv2.putText(faceFrame, "Face Stream", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 5)
