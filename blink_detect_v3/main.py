9f54d3a5c155e528767d99b984ff8d6f39fb60c6
Christian Harjuno <harjuno.tian@gmail.com>
2024-04-30 14:06:05 +0900

    Using landmark ratio calculation to detect blinks. More accurate compared to before

---
+++ blink_detect_v3/main.py
@@ -0,0 +1,117 @@
+# Handle imports and libraries
+from imutils.video import VideoStream
+from imutils import face_utils
+from scipy.spatial import distance as dist
+import datetime
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import numpy as np
+
+THRESHOLD=0.2
+CONSEC_FRAME=3
+COUNT=0
+TOTAL=0
+
+LOWEST_RATIO=1
+HIGHEST_RATIO=0
+
+# Handle face predictor
+print("Loading face landmark model")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# Handle input video stream
+print("Handling video input")
+stream = VideoStream(0).start()
+
+#Get eye indexes
+(lstart, lend) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rstart, rend) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# Define eye aspect ratio function
+def calc_EAR(eye):
+  leftPoint = dist.euclidean(eye[1], eye[5])
+  rightPoint = dist.euclidean(eye[2], eye[4])
+  horizontalDistance = dist.euclidean(eye[0], eye[3])
+
+  ratio = (leftPoint + rightPoint) / (2.0 * horizontalDistance) 
+  return ratio  
+
+# Process frames
+while True:
+  frame = stream.read()
+  frame = imutils.resize(frame, width=720)
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+  
+  face_landmark_stream = np.copy(gray)
+  face_landmark_stream = cv2.bilateralFilter(face_landmark_stream, 5, 1, 1)
+  isolate_stream = np.full_like(face_landmark_stream, 0)
+  
+  faces = detector(face_landmark_stream, 0)
+  for face in faces:
+    shape = predictor(face_landmark_stream, face)
+    shape = face_utils.shape_to_np(shape)
+    for(x, y) in shape:
+      cv2.circle(face_landmark_stream, (x, y), 1, (255, 255, 0), 2)
+      cv2.circle(isolate_stream, (x, y), 1, (255, 255, 0), 2)
+    
+    leftEye = shape[lstart:lend]
+    rightEye = shape[rstart:rend]
+    
+    leftRatio = calc_EAR(leftEye)
+    rightRatio = calc_EAR(rightEye)
+    
+    ratio = (leftRatio + rightRatio)/2.0
+    if(ratio > HIGHEST_RATIO):
+      HIGHEST_RATIO = ratio
+    if(ratio < LOWEST_RATIO):
+      LOWEST_RATIO = ratio
+    
+    if(ratio < THRESHOLD):
+      COUNT += 1
+    else:
+      if(COUNT >= CONSEC_FRAME):
+        TOTAL+=1
+      if(COUNT > HIGHEST_CONSEC):
+        HIGHEST_CONSEC = COUNT
+      if(COUNT < LOWEST_CONSEC):
+        LOWEST_CONSEC = COUNT
+      COUNT=0
+    
+    
+    cv2.putText(isolate_stream, "Blinks: {}".format(TOTAL), (20, 60), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
+    cv2.putText(isolate_stream, "Ratio: {}".format(ratio), (20, 90), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
+    cv2.putText(isolate_stream, "Left Ratio: {}".format(leftRatio), (20, 120), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
+    cv2.putText(isolate_stream, "Right Ratio: {}".format(rightRatio), (20, 150), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
+    cv2.putText(isolate_stream, "Consecutive Close: {}".format(COUNT), (20, 180), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
+    leftHull = cv2.convexHull(leftEye)
+    rightHull = cv2.convexHull(rightEye)
+    
+    cv2.drawContours(isolate_stream, [leftHull], -1, (255, 255, 0), 1)
+    cv2.drawContours(isolate_stream, [rightHull], -1, (255, 255, 0), 1)
+    cv2.drawContours(face_landmark_stream, [leftHull], -1, (255, 255, 0), 1)
+    cv2.drawContours(face_landmark_stream, [rightHull], -1, (255, 255, 0), 1)
+    
+    
+  cv2.putText(frame, "Original Feed", (20, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+  cv2.putText(gray, "Processed Feed", (20, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+  cv2.putText(face_landmark_stream, "Feed + Isolate", (20, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+  cv2.putText(isolate_stream, "Isolate", (20, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+    
+  horizontal_feed_0 = np.concatenate((frame, gray), axis=1)
+  horizontal_feed_1 = np.concatenate((isolate_stream, face_landmark_stream), axis=1)
+  vertical_feed = np.concatenate((horizontal_feed_0, horizontal_feed_1), axis=0)
+  cv2.imshow("Frame", vertical_feed)
+  
+  key = cv2.waitKey(1) & 0xFF
+  if(key == ord("q")):
+    break
+
+print(HIGHEST_RATIO)
+print(LOWEST_RATIO)
+cv2.destroyAllWindows()
+stream.stop()
